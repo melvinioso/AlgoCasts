2411a30f1ceb1cd2e8d808190bfdc64cf44c21c5
Matt McGavern <matt.mcgavern@gmail.com>
2022-05-31 16:35:58 -0500

    steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,36 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// Solution 1 - Iterative
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
+
+//     for (let col = 0; col < n; col++) {
+//       if (col <= row) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+
+//     console.log(stair);
+//   }
+// }
+
+// Solution 2 - Recursive
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  const add = stair.length <= row ? '#' : ' ';
+  steps(n, row, stair + add);
+}
 
 module.exports = steps;
