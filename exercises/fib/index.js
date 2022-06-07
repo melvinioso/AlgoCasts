f5e7c85ea7dbeaeabbb32a49d0c7c75d5773fe92
Matt McGavern <matt.mcgavern@gmail.com>
2022-06-07 16:46:54 -0500

    fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,44 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// Iterative solution - Linear runtime - O(n)
+// function fib(n) {
+//   const result = [0, 1];
+
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+
+//     result.push(a + b);
+//   }
+
+//   return result[n];
+// }
+
+// Improved recursive solution - memoization
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+// Recursive solution - Exponential runtime - O(2^n)
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+fib = memoize(fib);
 
 module.exports = fib;
